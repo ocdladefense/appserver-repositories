9fb1ffce73fc6bb7699a455324930e0445fd00f8
KlosheKopa <richardsa@my.lanecc.edu>
2022-02-08 12:34:26 -0800

    doc schema

--- module.php
+++ module.php
@@ -9,16 +9,35 @@ class ProductsModule extends Module {
     }
 
     public function doXML() {
-        $tpl = new Template("placeholder");
-        $tpl->addPath(__DIR__ . "/templates");
-
         $api = $this->loadForceApi();
 
         $result = $api->query("SELECT Id, Name, Description, ClickpdxCatalog__HtmlDescription__c, IsActive FROM Product2 WHERE IsActive = true AND ClickpdxCatalog__IsOption__c = false");
-        
-        var_dump($result->getRecords());
-        $html = $tpl;
 
-        return $tpl;
+        $dom = new DOMDocument('1.0', 'UTF-8');
+        $element = $dom->appendChild($dom->createElementNS('http://www.example.com/Docset','sphinx:docset'));
+        $schema = $element->appendChild($dom->createElement('sphinx:schema'));
+        $fieldOne = $schema->appendChild($dom->createElement('sphinx:field'));
+        $fieldOne->setAttribute('name', 'subject');
+        $fieldTwo = $schema->appendChild($dom->createElement('sphinx:field'));
+        $fieldTwo->setAttribute('name', 'content');
+        $fieldThree = $schema->appendChild($dom->createElement('sphinx:field'));
+        $fieldThree->setAttribute('name', 'product_name');
+        $attrOne = $schema->appendChild($dom->createElement('sphinx:attr'));
+        $attrOne->setAttribute('name', 'product_id');
+        $attrOne->setAttribute('type', 'string');
+        $attrTwo = $schema->appendChild($dom->createElement('sphinx:attr'));
+        $attrTwo->setAttribute('name', 'published');
+        $attrTwo->setAttribute('type', 'timestamp');
+        $attrThree = $schema->appendChild($dom->createElement('sphinx:attr'));
+        $attrThree->setAttribute('name', 'author_id');
+        $attrThree->setAttribute('type', 'int');
+        $attrThree->setAttribute('bits', '16');
+        $attrThree->setAttribute('default', '1');
+        //var_dump($dom);
+        
+        //var_dump($result->getRecords());
+
+
+        return $dom->saveXML();
     }
 }
