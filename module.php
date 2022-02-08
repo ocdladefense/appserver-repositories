2f61f0c02973ab1362400e5ff1316c75b06b5282
isaac walters <waltersi@my.lanecc.edu>
2022-02-08 10:35:54 -0800

    first query

--- module.php
+++ module.php
@@ -12,6 +12,11 @@ class ProductsModule extends Module {
         $tpl = new Template("placeholder");
         $tpl->addPath(__DIR__ . "/templates");
 
+        $api = $this->loadForceApi();
+
+        $result = $api->query("SELECT Id, Name, Description, ClickpdxCatalog__HtmlDescription__c, IsActive FROM Product2 WHERE IsActive = true AND ClickpdxCatalog__IsOption__c = false");
+        
+        var_dump($result->getRecords());
         $html = $tpl;
 
         return $tpl;
