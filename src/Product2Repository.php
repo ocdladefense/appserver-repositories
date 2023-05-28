4c67c0efd55afa02b0e99d67f06c0d27d09fa821
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2023-05-28 12:39:28 -0700

    Retrieve the title for the Product2 repository.

--- src/Product2Repository.php
+++ src/Product2Repository.php
@@ -28,6 +28,56 @@ class Product2Repository extends Repository {
 
 
 
+    public function getSchema() {
+
+        $schema = $this->createElement('sphinx:schema');
+          
+
+        $field_1 = $this->createElement('sphinx:field');
+        $field_1->setAttribute('name', 'subject');
+
+        $field_2 = $this->createElement('sphinx:field');
+        $field_2->setAttribute('name', 'content');
+
+        // $field_3 = $dom->createElement('sphinx:field');
+        // $field_3->setAttribute('name', 'name');
+
+        $attr_1 = $this->createElement('sphinx:attr');
+        $attr_1->setAttribute('name', 'alt_id');
+        $attr_1->setAttribute('type', 'string');
+
+        // http://sphinxsearch.com/docs/manual-2.3.2.html#conf-xmlpipe-attr-string
+        $title = $this->createElement('sphinx:attr');
+        $title->setAttribute('name', 'title');
+        $title->setAttribute('type', 'string');
+
+        // $attr_2 = $dom->createElement('sphinx:attr');
+        // $attr_2->setAttribute('name', 'published');
+        // $attr_2->setAttribute('type', 'timestamp');
+
+        $atindex = $this->createElement('sphinx:attr');
+        $atindex->setAttribute('name', 'indexname');
+        $atindex->setAttribute('type', 'string');
+
+        $attr_3 = $this->createElement('sphinx:attr');
+        $attr_3->setAttribute('name', 'author_id');
+        $attr_3->setAttribute('type', 'int');
+        $attr_3->setAttribute('bits', '16');
+        $attr_3->setAttribute('default', '1');
+
+
+        $schema->appendChild($field_1);
+        $schema->appendChild($field_2);
+        // $schema->appendChild($field_3);
+        $schema->appendChild($atindex);
+        $schema->appendChild($attr_1);
+        // $schema->appendChild($attr_2);
+        $schema->appendChild($attr_3);
+        $schema->appendChild($title);
+        
+        return $schema;
+    }    
+
 
 
 
@@ -52,6 +102,9 @@ class Product2Repository extends Repository {
             $subject = $this->createElement('subject');
             $subject->appendChild($this->createCDATASection($title));
 
+            $attr_title = $this->createElement('title');
+            $attr_title->appendChild($this->createCDATASection($title));
+
             $content = $this->createElement('content');
             $content->appendChild($this->createCDATASection($description));
 
@@ -62,6 +115,8 @@ class Product2Repository extends Repository {
             $doc->appendChild($content);
             $doc->appendChild($altId);
             $doc->appendChild($indexName);
+            $doc->appendChild($attr_title);
+            
 
             $nodes []= $doc;
             $start++;
